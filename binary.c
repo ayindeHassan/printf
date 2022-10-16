6345aa8c7b9a0e69e76b289c3a0194f2f7a8273d
Almuhandith <almuhandith1497@gmail.com>
2022-10-16 11:15:55 +0100

    binary

--- binary.c
+++ binary.c
@@ -8,30 +8,30 @@
  * @limit: size of the binary
  * Return: number of chars printed.
  */
-char *binary(char *bin, long int int_in, int isneg, int limit)
+char *fill_binary_array(char *binary, long int int_in, int isneg, int limit)
 {
 	int i;
 
 	for (i = 0; i < limit; i++)
-		bin[i] = '0';
-	bin[limit] = '\0';
+		binary[i] = '0';
+	binary[limit] = '\0';
 	for (i = limit - 1; int_in > 1; i--)
 	{
 		if (int_in == 2)
-			bin[i] = '0';
+			binary[i] = '0';
 		else
-			bin[i] = (int_in % 2) + '0';
+			binary[i] = (int_in % 2) + '0';
 		int_in /= 2;
 	}
 	if (int_in != 0)
-		bin[i] = '1';
+		binary[i] = '1';
 	if (isneg)
 	{
-		for (i = 0; bin[i]; i++)
-			if (bin[i] == '0')
-				bin[i] = '1';
+		for (i = 0; binary[i]; i++)
+			if (binary[i] == '0')
+				binary[i] = '1';
 			else
-				bin[i] = '0';
+				binary[i] = '0';
 	}
-	return (bin);
+	return (binary);
 }

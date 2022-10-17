4e03ec8f6b6ef22f0a4f0deb8c0d234d5987e147
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-10-17 18:24:51 +0100

    string

--- print_string.c
+++ print_string.c
@@ -12,18 +12,17 @@
 
 int print_string(va_list args)
 {
-	int i;
-	int count_fun = 0;
-	char *str = va_arg(args, char *);
+	int i, j;
+	char n[] = "(null)";
+	char *s = va_arg(args, char *);
 
-	if (!str)
-		str = "(null)";
-
-	if (str[0] == '\0')
-		return (-1);
-
-	for (i = 0; str[i] != '\0'; i++)
-		count_fun += _putchar(str[i]);
-
-	return (count_fun);
+	if (s == NULL)
+	{
+		for (i = 0; n[i] != '\0'; i++)
+			_putchar(n[i]);
+		return (6);
+	}
+	for (j = 0; s[j] != '\0'; j++)
+		_putchar(s[j]);
+	return (j);
 }

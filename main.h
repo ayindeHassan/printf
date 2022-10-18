57a8462a9192c9f34d17d54852f12db5c302ff5d
Hassan Toheeb <hassantoheebayinde@gmail.com>
2022-10-18 23:45:46 +0100

    task 7

--- main.h
+++ main.h
@@ -26,6 +26,13 @@ int print_specialHex(unsigned int c);
 int hex_pointer(unsigned long int c);
 int get_flags(const char *format, int *i);
 
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
 
 int print_char(va_list args);
 int print_digit(va_list args);
